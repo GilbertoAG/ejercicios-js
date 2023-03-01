3188aa6a735e3e0d4f110597f035be8bc4fcc4ff
Raven <glenin.aquino@gmail.com>
2023-02-28 23:00:21 -0600

    avance primeros dos retos

---
+++ script.js
@@ -0,0 +1,116 @@
+// Escribir una funcion que regrese la cantidad de valores true que hay en un array
+/*
+countTrue([true, false, false, true, false]) ➞ 2
+countTrue([false, false, false, false]) ➞ 0
+countTrue([]) ➞ 0
+ */
+// Regresar 0 si el array es vacio
+// usar recursion
+
+
+/* console.log ("Reto 1");
+function countTrue (array){ //funcion 
+    var count = 0;
+    for (var i = 0; i < array.length; i++){ //Contador for
+        if (array[i]){
+            count++;
+        }
+    }
+    return count;
+}
+
+
+//comprobacion
+console.log(countTrue([true, false, false, true, false])); //2
+console.log(countTrue([false, false, false, false])); // 0
+console.log(countTrue([])); //0 */
+
+
+console.log ("Reto 2");
+function countTrue (array){
+    if(array.length === 0) {
+        return 0;
+    } else if (array[0]) {
+        return 1 + countTrue(array.slice(1));
+    } else {
+        return countTrue(array.slice(1));
+    }
+}
+
+
+//comprobacion
+console.log(countTrue([true, false, false, true, false])); //2
+console.log(countTrue([false, false, false, false])); // 0
+console.log(countTrue([])); //0
+
+
+
+
+
+// Escribir una funcion para encontrar el mayor comun divisor de 2 numeros positivos enteros
+// Usar recursion
+
+console.log("Reto 2");
+function mayorCD (a, b){
+    if(b == 0){
+        return a;
+    }else {
+        return mayorCD(b, a % b);
+    }
+}
+
+//Comprobacion
+console.log(mayorCD(4, 6));
+console.log(mayorCD(20, 15));
+console.log(mayorCD(1, 33));
+
+
+// Encontrar los primeros n elementos de la serie fibonacci
+// Usar recursion
+
+// Encontrar el numero n de una figura piramidal triangular (tetrahedron)
+// (nivel) -> cantidad de numeritos
+
+// Crear una funcion que tome el nombre de un pais y su area
+// (nombre, a)
+// Debe regresar la proporcion del area del pais en acorde a el area de tierra en el planeta
+//('Russia', 17098242) -> Russia is 11.48%
+// ("USA", 9372610) -> "USA is 6.29
+
+// Escribir una funcion que retorne 0 si el input es 1 y 1 si el input es 0
+// No se pueden utilizar condicionales, ternarios, negaciones ni operatores bit
+
+// Messenger bzzz
+// (0 ) n
+// Si no hay nadie -> "No hay nadie en linea"
+// Si hay 1 persona -> "user1 esta en linea"
+// Si hay 2 personas -> "user1 y user2 estan en linea"
+// Si hay n>2 personas, "user1 y n-1 mas estan en linea"
+
+///////////////////////// Arrays
+
+// Encontrar el nombre de una sociedad secreta segun los datos de los nombres para cada integrante
+// secretName(["Felipe", "Fer", "Zabdiel"]) -> FFZ
+// secretName(['Phoebe', 'Ross', 'Chandler', 'Joey', 'Monica', 'Rachel']) -> CJMPRR
+// secretName(['Harry', 'Ron', 'Hermione']) -> HHR
+
+// onlineStatus(['mockIng99', 'J0eyPunch', 'glassedFer']) -> 'mockIng99, J0eyPunch and one 1 more online'
+
+// Crear una funcion que tome 2 parametros, (numero, longitud) y regrse un array de longitud cantidad de numeros multiplos del numero por parametro
+// arrayMultiplos(2, 10) -> [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+// arrayMultiplos(17, 6) -> [17, 34, 51, 68, 85, 102]
+
+
+
+
+// Escrbir una funcion que descubra si el array es dominante positivo
+// Un array es positivo dominante cuando la mayoria de sus elementos son positivos -> [1, -2, 55, 10]
+// positiveDom([-1, -3, -5, 4, 6767]) -> false
+
+
+// Promedio antipode
+// Dado un array, devolver un array de menor longitud de acuerdo a los siguentes pasos:
+// - Dividir el array en 2 partes iguales*. Si no son iguales, eliminar el elemento en medio para obtener 2 arrays iguales
+// - Sumar cada numero de la primera parte con los numeros inversos de la segunda parte
+// [1,2,3] [5,22,6] -> 1 + 6, 2 + 22, 3 + 5 -> [7, 24, 8]
+// - Dividir cada numero del array final entre 2  -> [3.5, 12, 4]
